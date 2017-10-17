ea971e7a1b58fdfb23c4228c329962e1a8a7a292
Mauro <maurocerbai@gmail.com>
2017-10-17 23:42:38 +0200

    Completed training and some predictions

--- titanic.py
+++ titanic.py
@@ -4,18 +4,28 @@
 
 import csv
 import pandas
+import numpy
+import scipy
+import types
+import math
+NumberTypes = (types.IntType, types.LongType, types.FloatType, types.ComplexType)
+
 colnames = ['number', 'name', 'clas', 'age', 'sex', 'survived', 'sexcode']
 datad = pandas.read_csv('titanic_data.csv', names=colnames)
 
-#remove first line of data because it's a header
+#remove first row because it's a header
 data = datad.iloc[1:]
 
 
-data = data[:5] ###################REMOVE THIS TODO
+#data = data[:13] ###################REMOVE THIS TODO
+
 
 #extract survived information and use as a label
 labels = data.survived.tolist()
-print labels
+for l in labels:
+	l = float(l)
+	#print isinstance(l, NumberTypes)
+#print labels
 print "-"
 
 
@@ -26,71 +36,71 @@ del data['sex']
 del data['survived']
 feature = data.values.tolist()
 for f in feature:
+	#print f[0]
 	f[0] = f[0][0]
-print feature
+	#print f[0]
+	if f[0] == "*":
+		f[0] = -1
+	f[0] =  float(f[0])
+	#print f[0]
+
+	f[1] =  float(f[1])
+	if math.isnan(f[1]):
+		f[1] = 0
+	#print f[1]
+
+	f[2] =  float(f[2])
+	#print f[2]
+	
+	#print isinstance(f[0], NumberTypes)
+	#print isinstance(f[1], NumberTypes)
+	#print isinstance(f[2], NumberTypes)
+#print feature
 print "--"
 
 
-
 from sklearn import model_selection
-feature_train, feature_test, label_train, label_test = model_selection.train_test_split(feature, labels)#, shuffle=False)
-print feature_train
-print label_train
-print
-print feature_test
-print label_test
-print "---"
-
-
-
-#clf = DecisionTreeClassifier()
-#clf.fit(features_train, labels_train)
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(feature, labels)#, shuffle=False)
+#print features_train
+#print labels_train
+#print
+#print features_test
+#print labels_test
+#print "---"
 
 
 from sklearn import svm
-clf = svm.LinearSVC()
-clf.fit(feature_train, label_train)
-print(clf.coef_)
-print(clf.intercept_) 
-
-#print "Accuracy on trainig data", clf.score(feature_train, label_train)
-#print "Accuracy on test data", clf.score(feature_test,label_test)
-
+clf_svc = svm.LinearSVC()
+clf_svc.fit(features_train, labels_train)
+print "--LinearSVC--"
+print "Accuracy on trainig data", clf_svc.score(features_train, labels_train)
+print "Accuracy on test data", clf_svc.score(features_test,labels_test)
+print
 
 
-
-
-
-
-
-
+from sklearn.tree import DecisionTreeClassifier
+clf_dt = DecisionTreeClassifier()
+clf_dt.fit(features_train, labels_train)
+print "--DecisionTree--"
+print "Accuracy on train: ", clf_dt.score(features_train, labels_train)
+print "Accuracy on test: ", clf_dt.score(features_test, labels_test)
 
 
 
+print
+print "Predicting male of 26yo travelling in 3rd class"
+print(clf_dt.predict([[3.0, 26.0, 0.0]]))
+print "Predicting female of 26yo travelling in 3rd class"
+print(clf_dt.predict([[3.0, 26.0, 1.0]]))
 
+print
+print "Predicting male of 26yo travelling in 2rd class"
+print(clf_dt.predict([[2.0, 26.0, 0.0]]))
+print "Predicting female of 26yo travelling in 2rd class"
+print(clf_dt.predict([[2.0, 26.0, 1.0]]))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### Task 1: Select what features you'll use.
-### Task 2: Remove outliers
-### Task 3: Create new feature(s)
-### Task 4: Try a varity of classifiers
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
+print
+print "Predicting male of 26yo travelling in 1rd class"
+print(clf_dt.predict([[1.0, 26.0, 0.0]]))
+print "Predicting female of 26yo travelling in 1rd class"
+print(clf_dt.predict([[1.0, 26.0, 1.0]]))
